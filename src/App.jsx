fb7a97fa557a32eeaa6ce06ac23e2b0f08bc5a04
MisbahAnjumShaikh <misbahanjumshaikh05pk@gmail.com>
2025-01-31 17:51:49 +0500

    Initialize Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import { Fragment } from 'react'
+import './App.css'
+import Navbar from './assets/components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <Fragment>
+      <Navbar/>
+    </Fragment>
+  )
+}
+
+export default App
